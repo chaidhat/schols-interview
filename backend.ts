def97d89064e176174f228342750cde68742bb89
chaidhat <chaimongkol@ucla.edu>
2025-08-03 13:51:13 -0700

    fix: clarify backend prompt

--- backend.ts
+++ backend.ts
@@ -15,13 +15,50 @@ app.use(express.json());
 
 
 
+/*
+1. There are multiple tasks per user. The tasks can be stored as an in-memory store.
+
+2. These tasks are compute heavy and take three seconds to complete. 
+
+3. The server should only process one task at a time. NO task can run concurrently.
+
+task t1 created at t=0, 
+task t2 created at t=1,
+task t3 created at t=3
+
+# - processing
+. - queued
+O - task created
+
+CORRECT
+   t=0   t=1   t=2   t=3               t=6               t=9
+t1 O#####|#####|#####|     |     |     |     |     |     |     |
+t2 |     O.....|.....|#####|#####|#####|     |     |     |     |
+t2 |     |     |     O.....|.....|.....|#####|#####|#####|     |
+
+
+INCORRECT
+   t=0   t=1   t=2   t=3               t=6               t=9
+t1 O#####|#####|#####|     |     |     |     |     |     |     |
+t2 |     O#####|#####|#####|     |     |     |     |     |     |
+t3 |     |     |     O#####|#####|#####|     |     |     |     |
+*/
+
+// This is a task
+interface Task {
+    taskId: string,
+    taskName: string,
+    completed: boolean,
+    userId: string,
+}
+
 // does a task
-async function doTask() {
+async function doTask(task: Task) {
     await new Promise(resolve => setTimeout(resolve, 3000));
 }
 
 // TODO: replace any with the correct type
-function createTask(taskName: any, userId: any): void {
+function createTask(taskName: string, userId: string): void {
 
     // TODO: implement
 
@@ -29,16 +66,12 @@ function createTask(taskName: any, userId: any): void {
 }
 
 // TODO: replace any with the correct type
-function getTasks(userId: any): any {
+function getTasks(userId: string):  Task[] {
     let taskId, taskName, completed: any;
 
     // TODO: implement
 
-    return [{
-        "taskId": taskId,
-        "taskName": taskName,
-        "completed": completed
-    }];
+    return [];
 }
 
 
